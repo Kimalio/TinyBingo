f86e9738ff93de8a76b0768a70a4f3f3b1006b2c
Kimalio <95563965+Kimalio@users.noreply.github.com>
2025-08-09 03:43:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: '', // для GitHub Pages в корневом репо. Если gh-pages из /docs — поменять.
+    base: '/TinyBingo/'
+    plugins: [react()]
 })
