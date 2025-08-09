cfcc3253679957e066c56b11b28952777d5a3f8a
Kimalio <95563965+Kimalio@users.noreply.github.com>
2025-08-09 03:58:51 +0200

    cnfg
    minor updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '', // для GitHub Pages в корневом репо. Если gh-pages из /docs — поменять.
+    base: '/TinyBingo/',
+    build: {
+        outDir: 'docs'
+    }
 })
